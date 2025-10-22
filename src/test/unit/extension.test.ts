483453ba478228c42fb4d6dff8eef5826b402f38
ark-65 <liuwufangzhou@vip.qq.com>
2025-10-22 10:39:49 +0800

    test: add unit test and update docs

--- src/test/unit/extension.test.ts
+++ src/test/unit/extension.test.ts
@@ -1,5 +1,6 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import * as extension from '../../extension';
 
 describe('Extension', () => {
     // Note: Extension activation tests require VSCode extension host environment
@@ -7,12 +8,10 @@ describe('Extension', () => {
 
     describe('module structure', () => {
         it('should export activate function', () => {
-            const extension = require('../../extension');
             expect(extension.activate).to.be.a('function');
         });
 
         it('should export deactivate function', () => {
-            const extension = require('../../extension');
             expect(extension.deactivate).to.be.a('function');
         });
     });
